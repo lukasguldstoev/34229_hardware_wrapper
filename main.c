ba6f854a32c779d886b66227bd6a1de32041f76f
lukasguldstoev <lukas@mittun.dk>
2022-06-07 12:39:45 +0200

    eww

--- main.c
+++
@@ -1,6 +0,0 @@
-#include "http-client-c/src/http-client-c.h"
-
-int main(){
-    struct parsed_url *purl = parse_url("http://www.google.com/");
-    struct http_response *hrep = http_req("GET / HTTP/1.1\r\nHostname:www.google.com\r\nConnection:close\r\n\r\n", purl);
-}
